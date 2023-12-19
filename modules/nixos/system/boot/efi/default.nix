d91361531e1e14d5c64bd036ffc365dd6b8aa006
Knut Holtet <knutholtet@live.no>
2023-12-19 15:51:28 +0100

    fixes

--- modules/nixos/system/boot/efi/default.nix
+++ modules/nixos/system/boot/efi/default.nix
@@ -35,8 +35,5 @@ in {
 
     # https://github.com/NixOS/nixpkgs/blob/c32c39d6f3b1fe6514598fa40ad2cf9ce22c3fb7/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix#L66
     boot.loader.systemd-boot.editor = false;
-
-
-    boot.kernelPackages = pkgs.linuxKernel.packages.linux_testing;
   };
 }
